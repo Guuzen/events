7dd7f2c798238cb5bd1df14f93ca5af013d2252f
Guuzen <Guuzen@users.noreply.github.com>
2020-08-27 23:59:05 +0300

    Openapi controller moved to openapi module

--- src/Infrastructure/Http/Openapi/GetOpenapiDocHttpAdapter.php
+++ src/Infrastructure/Http/Openapi/GetOpenapiDocHttpAdapter.php
@@ -2,10 +2,9 @@
 
 declare(strict_types=1);
 
-namespace App;
+namespace App\Infrastructure\Http\Openapi;
 
 use App\Infrastructure\Http\AppController\AppController;
-use App\Infrastructure\Http\Openapi\OpenapiSchema;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
